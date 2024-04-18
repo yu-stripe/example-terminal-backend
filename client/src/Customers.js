8361f12936551a18a64c1487764a1a76822caff4
Yu Kitazume <yu@stripe.com>
2024-04-18 15:29:58 +0900

    add production url

--- client/src/Customers.js
+++ client/src/Customers.js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { API_URL } from './index.js'
 
 export default function Customers() {
-  const API = process.env.TERMINAL_BACKEND_API
   const [customers, setCustomers] = useState([]);
 
   useEffect(() => {
-    fetch("https://example-terminal-backend-l8i6.onrender.com/api/customers").then(async(r) => {
+    fetch(`${API_URL}/api/customers`).then(async(r) => {
       const { data } = await r.json();
       setCustomers(data);
     });
