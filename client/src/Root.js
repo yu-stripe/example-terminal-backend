1f6254fae496ba884c5148a11ad244ac2107013e
Yu Kitazume <yu@stripe.com>
2024-04-19 12:32:13 +0900

    add path

--- client/src/Root.js
+++ client/src/Root.js
@@ -6,18 +6,12 @@ export default function App() {
   return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Stripe Termianl Demo App
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div>
+            <p><a href="/customers">顧客リスト</a></p>
+          </div>
         </header>
       </div>
   )
