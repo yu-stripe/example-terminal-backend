2d5717eb9a5d905940486e111fd0fa9b7c20dc72
Yu Kitazume <yu@stripe.com>
2024-04-20 12:14:24 +0900

    change api

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//export const API_URL = "https://example-terminal-backend-l8i6.onrender.com" 
-export const API_URL = "http://localhost:4567"
+export const API_URL = "https://example-terminal-backend-l8i6.onrender.com" 
+//export const API_URL = "http://localhost:4567"
 export const STRIPE_KEY = "pk_test_51Oh4JADoruLo2b1wGQxY3JEp9oCp3YCeWMjkET5YCSzDpNduUXGZRV7gVEYx47nIMKeT3d6HQCVCTZxYBPVlxtIS00a5dXFwoX"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
